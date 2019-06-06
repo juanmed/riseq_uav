758c0bbd8a50e4795b0d5f73369d12487fb15f17
egauh0113 <egauh@naver.com>
2019-06-06 12:33:04 +0900

    added MIT License

--- riseq_perception/src/riseq_perception_node.cpp
+++ riseq_perception/src/riseq_perception_node.cpp
@@ -1,5 +1,27 @@
-/// This node is for apart and cluster several obstacles(e.g. poles, trees) at AI R&D Grand Challenge.
-/// Using PCL library, process Euclidean clustering and plane segmentation so that can detect obstacles from stereo depth.
+/**
+@author  Eugene Auh
+@version 0.1.0
+@brief This node is for apart and cluster several obstacles(e.g. poles, trees) at AI R&D Grand Challenge.
+       Using PCL library, process Euclidean clustering and plane segmentation so that can detect obstacles from stereo depth.
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the ""Software""), to deal in the 
+Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the 
+following conditions:
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 
 #include <ros/ros.h>
 // PCL specific includes. PCL <-> PointCloud2(ROS message)
