e57cb5365037437f46de0da22a65d35459e3420e
yonghee93 <pdydgml93@google.com>
2019-07-08 17:04:25 +0900

    integrating code

--- riseq_tests/scripts/mavros_flight/flight_test2.py
+++ riseq_tests/scripts/mavros_flight/flight_test2.py
@@ -2,6 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import AttitudeTarget
 from mavros_msgs.srv import CommandBool
 from mavros_msgs.srv import SetMode
 from mavros_msgs.msg import State
@@ -11,17 +12,18 @@ import tf
 import numpy as np
 
 # mode : [ Start, Window, Pole, Pipe, Tree, Net, Wind]
-mode = "Pole"
+# pub_mode : [ Position, Attitude ]
+mode = "Start"
+pub_mode = "Position"
 current_state = State()
 waypoint = []
 receive_path = False
-position = np.zeros(2)
+position = np.zeros(3)
 
 
 def state_cb(msg):
     global current_state
     current_state = msg
-    print "callback"
 
 
 def path_cb(msg):
@@ -45,6 +47,7 @@ def pose_cb(msg):
     global position
     position[0] = msg.pose.position.x
     position[1] = msg.pose.position.y
+    position[2] = msg.pose.position.z
 
 
 if __name__ == "__main__":
@@ -59,29 +62,34 @@ if __name__ == "__main__":
     rospy.Subscriber("riseq/planning/uav_waypoint", Path, path_cb)
     rospy.Subscriber("mavros/local_position/pose", PoseStamped, pose_cb)
     local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
-    print" Publisher and Subscriber Created"
+    att_raw_pub = rospy.Publisher('mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=10)
+    print "Publisher and Subscriber Created"
 
     arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
     set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
     landing_client = rospy.ServiceProxy('mavros/cmd/land', CommandTOL)
-
     print "Clients Created"
+
     rate = rospy.Rate(20)
 
     while not current_state.connected:
         print "connected is False"
         rate.sleep()
 
-    while not receive_path:
-        print "waiting path"
-        rate.sleep()
+    #while not receive_path:
+    #    print "waiting path"
+    #    rate.sleep()
 
     print("Creating pose")
     pose = PoseStamped()
     # set position here
-    pose.pose.position.x = waypoint[0][0]
-    pose.pose.position.y = waypoint[0][1]
+    pose.pose.position.x = 0
+    pose.pose.position.y = 0
     pose.pose.position.z = 0
+
+    att = AttitudeTarget()
+    att.thrust = 0.57
+    rospy.loginfo(att)
 
     for i in range(100):
         local_pos_pub.publish(pose)
@@ -95,8 +103,10 @@ if __name__ == "__main__":
 
     last_request = rospy.Time.now()
     mode = "Start"
+    pub_mode = "Position"
     start_x = 0
     start_y = 0
+    start_z = 0
     iteration = 0
 
     while not rospy.is_shutdown():
@@ -112,7 +122,10 @@ if __name__ == "__main__":
                 print("Vehicle armed")
             last_request = rospy.Time.now()
 
-        local_pos_pub.publish(pose)
+        if pub_mode == "Position":
+            local_pos_pub.publish(pose)
+        elif pub_mode == "Attitude":
+            att_raw_pub.publish(att)
         # print current_state
         rate.sleep()
 
@@ -120,34 +133,61 @@ if __name__ == "__main__":
                 5.0):
             if mode == "Start":
                 if iteration == 0:
-                    print "Start"
+                    print "Start Mode"
+                    pub_mode = "Position"
                     iteration = iteration + 1
 
-                if iteration < 100:
-                    pose.pose.position.z = 0.7 * iteration / 100
-                    iteration = iteration + 1
-
-                if iteration == 100:
+                if rospy.Time.now() - last_request < rospy.Duration(15.0):
+                    if iteration == 1:
+                        print "Start Mode: hovering"
+                        iteration = iteration + 1
+                    pose.pose.position.z = 2 * (
+                                rospy.Time.now() - last_request - rospy.Duration(5.0)) / rospy.Duration(10.0)
+                elif rospy.Time.now() - last_request < rospy.Duration(25.0):
+                    if iteration == 2:
+                        print "Start Mode: go left to find window"
+                        start_x = position[0]
+                        start_y = position[1]
+                        iteration = iteration + 1
+                    pose.pose.position.x = start_x
+                    pose.pose.position.y = start_y - 1 * (
+                            rospy.Time.now() - last_request - rospy.Duration(15.0)) / rospy.Duration(10.0)
+                    pose.pose.position.z = 2
+                else:
+                    print "Start Mode Over"
+                    mode = "Window"
                     iteration = 0
                     last_request = rospy.Time.now()
-                    mode = "Window"
 
             elif mode == "Window":
-
                 if iteration == 0:
                     print "Window mode"
+                    pub_mode = "Attitude"
                     iteration = iteration + 1
 
-                mode = "Pole"
-                iteration = 0
-                last_request = rospy.Time.now()
+                '''
+                Juan's code for detecting window, generating trajectory, control drone.
+                '''
+                if rospy.Time.now() - last_request < rospy.Duration(5.0):
+                    pass
 
+                else:
+                    print "Window mode Over"
+                    mode = "Pole"
+                    iteration = 0
+                    last_request = rospy.Time.now()
             elif mode == "Pole":
-
                 if iteration == 0:
                     print "Pole mode"
+                    pub_mode = "Position"
                     start_x = position[0]
                     start_y = position[1]
+                    start_z = position[2]
+                    pose.pose.position.x = start_x
+                    pose.pose.position.y = start_y
+                    pose.pose.position.z = start_z
+                    rospy.loginfo(pose)
+
                     iteration = iteration + 1
 
                 # To Construct Octomap, it moves left and right for 30 seconds
@@ -159,7 +199,7 @@ if __name__ == "__main__":
                         iteration = iteration + 1
                     pose.pose.position.y = start_y - 1 * (
                                 rospy.Time.now() - last_request - rospy.Duration(5.0)) / rospy.Duration(10.0)
-                    pose.pose.position.z = 0.7
+                    #pose.pose.position.z = 0.7
                 elif rospy.Time.now() - last_request < rospy.Duration(25.0):
                     if iteration == 2:
                         print "go left"
@@ -169,17 +209,17 @@ if __name__ == "__main__":
                     pose.pose.position.x = start_x
                     pose.pose.position.y = start_y + 2 * (
                                 rospy.Time.now() - last_request - rospy.Duration(15.0)) / rospy.Duration(10.0)
-                    pose.pose.position.z = 0.7
+                    #pose.pose.position.z = 0.7
                 elif rospy.Time.now() - last_request < rospy.Duration(35.0):
                     if iteration == 3:
                         print "go origin"
                         #start_x = position[0]
                         start_y = position[1]
                         iteration = iteration + 1
-                    pose.pose.position.x = start_x
+                    #pose.pose.position.x = start_x
                     pose.pose.position.y = start_y - 1 * (
                                 rospy.Time.now() - last_request - rospy.Duration(25.0)) / rospy.Duration(10.0)
-                    pose.pose.position.z = 0.7
+                    #pose.pose.position.z = 0.7
                 else:
                     print "follow path"
                     break
