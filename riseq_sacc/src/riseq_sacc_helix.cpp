a7297d065236af4a672839ab6c900b3fd6bb3096
JIksoo <dlrtn527@naver.com>
2019-09-02 14:47:28 +0900

    box_color

--- riseq_sacc/src/riseq_sacc_helix.cpp
+++ riseq_sacc/src/riseq_sacc_helix.cpp
@@ -7,7 +7,11 @@
 #include <sensor_msgs/image_encodings.h>
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <mavros_msgs/GlobalPositionTarget.h>
 #include <math.h>
+
+ros::Publisher helix_point_pub;
+mavros_msgs::GlobalPositionTarget point;
 
 float lat, lon, alt, heading;
 
@@ -64,17 +68,20 @@ float getAverageDistance(const sensor_msgs::Image::ConstPtr& msg, int& left, int
 void DepthCallback(const sensor_msgs::Image::ConstPtr& msg) {
    cv_bridge::CvImagePtr cv_ptr;
    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
-   cv::Mat img = cv_ptr->image;
+   cv::Mat depth_img = cv_ptr->image;
    cv::Mat thr_img;
 
-   cv::threshold(img, thr_img, 0.8, 255, cv::THRESH_BINARY_INV);
+   cv::threshold(depth_img, thr_img, 0.8, 255, cv::THRESH_BINARY_INV);
 
    thr_img.convertTo(thr_img, CV_8UC1);
+
+   cv::resize(thr_img, thr_img, cv::Size(640,360), 0, 0, CV_INTER_NN);
+   cv::Mat display_img = thr_img;
 
    /* test depth distance 
    std::cout<<thr_img.at<uchar>(640,360)<<std::endl;
    std::cout<<img.at<float>(640,360)<<std::endl;
-   cv::circle(thr_img, cv::Point(640,360), 3, cv::Scalar(0,0,255), 1);
+   cv::circle(thr_img, cv::Point(640,360), 3, 100, 1);
    */
 
    cv::Mat labels, stats, centroids;
@@ -85,8 +92,8 @@ void DepthCallback(const sensor_msgs::Image::ConstPtr& msg) {
    for (int i = 0; i<n; i++){
        if (i < 1) continue;
        int height = stats.at<int>(i, cv::CC_STAT_HEIGHT);
-       float ratio = stats.at<int>(i, cv::CC_STAT_HEIGHT) / stats.at<int>(i, cv::CC_STAT_WIDTH);
-       if (height_max < height && height >= 450 && ratio > 1){
+       //float ratio = stats.at<int>(i, cv::CC_STAT_HEIGHT) / stats.at<int>(i, cv::CC_STAT_WIDTH);
+       if (height_max < height && height >= 180){
            height_max = height;
            i_max = i;
        }
@@ -99,15 +106,10 @@ void DepthCallback(const sensor_msgs::Image::ConstPtr& msg) {
       int top = stats.at<int>(i_max,cv::CC_STAT_TOP);
       int width = stats.at<int>(i_max,cv::CC_STAT_WIDTH);
       int height = stats.at<int>(i_max,cv::CC_STAT_HEIGHT);
-
-      float avgdist = getAverageDistance(msg, left, top, width, height);
-      //ROS_INFO_STREAM("Avg Dist:" << avgdist);
-
-      cv::rectangle(thr_img, cv::Point(left,top), cv::Point(left+width,top+height),cv::Scalar(66,255,5),3);
+      cv::rectangle(display_img, cv::Point(left,top), cv::Point(left+width,top+height),100,3);
    }
 
-   cv::imshow("depth", thr_img);
-
+   cv::imshow("depth", display_img);
    cv::waitKey(1);
 
 }
@@ -124,13 +126,13 @@ void headingCallback(const std_msgs::Float64::ConstPtr& msg){
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "rise_sacc_helix");
-    ros::NodeHandle n1, n2, n3;
+    ros::NodeHandle n1, n2, n3, n4;
     image_transport::ImageTransport it(n1);
-    cv::namedWindow("depth", cv::WINDOW_AUTOSIZE);
 
     image_transport::Subscriber Depth  = it.subscribe("/zed/zed_node/depth/depth_registered", 1, DepthCallback);
     ros::Subscriber gps_sub = n2.subscribe("/mavros/global_position/global",1, gpsCallback);
     ros::Subscriber heading_sub = n3.subscribe("/mavros/global_position/compass_hdg",1, headingCallback);
+    helix_point_pub = n4.advertise<mavros_msgs::GlobalPositionTarget>("/setpoint_helix",1);
 
     if (!ros::ok()){
         cv::destroyAllWindows();
